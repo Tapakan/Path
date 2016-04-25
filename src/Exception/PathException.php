e4411919e11cd149ed9953645df3864328152146
Alex <t_tapak@yahoo.com>
2016-04-25 22:52:03 +0300

    PathException

---
+++ src/Exception/PathException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PathException class.
+ * @package     eva\exception
+ * @version     1.0
+ * @license     http://mit-license.org/
+ * @author      Tapakan https://github.com/Tapakan
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+namespace eva\exception;
+
+use Exception;
+
+class PathException extends Exception
+{
+    
+}
